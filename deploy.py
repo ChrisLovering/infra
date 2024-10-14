d01c9a2ba9855b9b76f306a4ff109c38c2e7a7b3
Chris Lovering <chris.lovering.95@gmail.com>
2024-10-14 18:58:27 +0100

    Create a fail2ban task

--- deploy.py
+++ deploy.py
@@ -2,3 +2,4 @@ from pyinfra import local
 
 local.include("tasks/common/main.py")
 local.include("tasks/users/main.py")
+local.include("tasks/fail2ban/main.py")
---
+++ tasks/fail2ban/main.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+
+from pyinfra.operations import apt, files, systemd
+
+template_dir = Path(__file__).parent / "templates"
+
+apt.packages(
+    name="Install fail2ban",
+    packages=["fail2ban"],
+    present=True
+)
+
+create_fail2ban_config = files.template(
+    name="Create fail2ban config",
+    src=template_dir / "jail.local.j2",
+    dest="/etc/fail2ban/jail.local",
+    user="root",
+    group="root",
+    mode="0444",
+)
+if create_fail2ban_config.changed:
+    systemd.service("fail2ban", reloaded=True)
+
+systemd.service(
+    name="Start fail2ban service",
+    service="fail2ban",
+    running=True,
+    enabled=True,
+)
