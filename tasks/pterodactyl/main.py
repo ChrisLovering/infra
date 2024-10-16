403c852e8a9c409f6762906ca9fe103bbe0845fe
Chris Lovering <chris.lovering.95@gmail.com>
2024-10-16 20:07:18 +0100

    Always create the admin user as an admin

--- tasks/pterodactyl/main.py
+++ tasks/pterodactyl/main.py
@@ -3,3 +3,4 @@ from pyinfra import local
 local.include("tasks/pterodactyl/dependencies.py")
 local.include("tasks/pterodactyl/panel_setup.py")
 local.include("tasks/pterodactyl/panel_webserver.py")
+local.include("tasks/pterodactyl/wings.py")
